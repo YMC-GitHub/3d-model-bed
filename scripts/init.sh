dd06092ce5c70362e407b0507168bba5b99667a2
yemiancheng <ymc.github@gmail.com>
2020-02-18 18:54:20 +0800

    build: update init sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,19 +8,12 @@ source "$THIS_FILE_PATH/path-resolve.sh"
 
 PROJECT_PATH=$(path_resolve "$THIS_FILE_PATH" "../")
 
-echo "${PROJECT_PATH}/dist/material"
 mkdir -p "${PROJECT_PATH}/dist/material"
-echo "${PROJECT_PATH}/dist/model"
 mkdir -p "${PROJECT_PATH}/dist/model"
-echo "${PROJECT_PATH}/dist/texture"
 mkdir -p "${PROJECT_PATH}/dist/texture"
-echo "${PROJECT_PATH}/dist/uv-img"
 mkdir -p "${PROJECT_PATH}/dist/uv-img"
-echo "${PROJECT_PATH}/src"
 mkdir -p "${PROJECT_PATH}/src"
-echo "${PROJECT_PATH}/note"
 mkdir -p "${PROJECT_PATH}/note"
-echo "${PROJECT_PATH}/screenshot"
 mkdir -p "${PROJECT_PATH}/screenshot"
 #file-usage
 # ./scripts/init.sh
